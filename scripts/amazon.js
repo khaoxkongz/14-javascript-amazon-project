bac7e19f9b6f93c5b5c70b9a0a552251059a178d
khaoxkongz <thanapat.makkawal@gmail.com>
2024-02-03 16:59:39 +0700

    Added message with object

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -41,7 +41,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -68,6 +68,19 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 			}
 		});
 
+    // We're going to use an object to save the timeout ids.
+    // The reason we use an object is because each product
+    // will have its own timeoutId. So an object lets us
+    // save multiple timeout ids for different products.
+    // For example:
+    // {
+    //   'product-id1': 2,
+    //   'product-id2': 5,
+    //   ...
+    // }
+    // (2 and 5 are ids that are returned when we call setTimeout).
+    const addedMessageTimeouts = {};
+
     const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
     const quantity = Number(quantitySelector.value);
 
@@ -90,5 +103,22 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     document.querySelector('.js-cart-quantity')
       .innerHTML = cartQuantity;
 
+    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+    addedMessage.classList.add('added-to-cart-visible');
+
+    // Check if there's a previous timeout for this
+    // product. If there is, we should stop it.
+    const previousTimeoutId = addedMessageTimeouts[productId];
+    if (previousTimeoutId) {
+      clearTimeout(previousTimeoutId);
+    }
+
+    const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+
+    // Save the timeoutId for this product
+    // so we can stop it later if we need to.
+    addedMessageTimeouts[productId] = timeoutId;
 	});
 });
