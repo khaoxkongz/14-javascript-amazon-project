ba0173e3c0e02c404ebc51eb3ea70456d4df02c6
khaoxkongz <thanapat.makkawal@gmail.com>
2024-02-03 13:26:31 +0700

    Add quantity selector input interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -67,12 +67,16 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 			}
 		});
 
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+    const quantity = Number(quantitySelector.value);
+    console.log(quantity);
+
 		if (matchingItem) {
-			matchingItem.quantity += 1;
+			matchingItem.quantity += quantity;
 		} else {
 			cart.push({
 				productId: productId,
-				quantity: 1,
+				quantity: quantity,
 			});
 		}
 
@@ -81,6 +85,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     cart.forEach((item) => {
       cartQuantity += item.quantity;
     })
+
+    console.log(cart);
 
     document.querySelector('.js-cart-quantity')
       .innerHTML = cartQuantity;
